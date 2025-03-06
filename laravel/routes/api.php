4d17e5098038b3f15f88884758b0b07f8031c1ec
lengkimtry <kimt47494@gmail.com>
2025-03-06 23:47:33 +0700

    Add API routes for Product and Category controllers with CRUD operations

---
+++ laravel/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::controller(CategoryController::class)->prefix('categories')->group(function(){
+    Route::get('/', 'getCategories'); // Get all categories
+    Route::post('/', 'createCategory'); // Create 1 category
+    Route::get('/{categoryId}', 'getCategory'); // Get 1 category by categoryId
+    Route::patch('/{categoryId}', 'updateCategory'); // Update 1 category
+    Route::delete('/{categoryId}', 'deleteCategory'); // Delete 1 category
+    Route::get('/{categoryId}/products', 'getCategoryProducts'); // Get all products belong to categoryId
+});
+
+Route::controller(ProductController::class)->prefix('products')->group(function(){
+    Route::get('/', 'getProducts');
+    Route::post('/', 'createProduct');
+    Route::get('/{productId}', 'getProduct');
+    Route::patch('/{productId}', 'updateProduct');
+    Route::delete('/{productId}', 'deleteProduct');
+});
