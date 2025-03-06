c43fb6e041f96405a0b199a9afcefa85ed392388
lengkimtry <kimt47494@gmail.com>
2025-03-06 23:46:34 +0700

    Add ProductController with CRUD operations for products

---
+++ laravel/app/Http/Controllers/ProductController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function getProducts()
+    {
+        $products = Product::all();
+        return response()->json([
+            'message' => 'Get all products',
+            'data' => $products
+        ]);
+    }
+
+    public function createProduct(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'category_id' => 'required|integer|exists:categories,id',
+        ]);
+        $product = Product::create($request->all());
+        return response()->json($product, 201);
+    }
+
+    public function getProduct($id)
+    {
+        return Product::findOrFail($id);
+    }
+
+    public function updateProduct(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'price' => 'required|numeric',
+            'category_id' => 'required|integer|exists:categories,id',
+        ]);
+        $product = Product::findOrFail($id);
+        $product->update($request->all());
+        return response()->json($product, 200);
+    }
+
+    public function deleteProduct($id)
+    {
+        Product::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
+}
